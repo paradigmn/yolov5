46c426c8b7e0b51561024731114c13ddcf39e941
paradigm <RubenProkscha@gmail.com>
2022-03-12 11:07:33 +0100

    replaced reshape ops with split

--- models/tf.py
+++ models/tf.py
@@ -222,19 +222,27 @@ class TFDetect(keras.layers.Layer):
             x.append(self.m[i](inputs[i]))
             # x(bs,20,20,255) to x(bs,3,20,20,85)
             ny, nx = self.imgsz[0] // self.stride[i], self.imgsz[1] // self.stride[i]
-            x[i] = tf.reshape(x[i], [-1, ny * nx, self.na, self.no])
 
             if not self.training:  # inference
                 y = tf.sigmoid(x[i])
+                y = tf.split(y, self.na, axis=-1)
                 grid = tf.transpose(self.grid[i], [0, 2, 1, 3]) - 0.5
+                grid = tf.reshape(grid, [-1, ny, nx, 2])
                 anchor_grid = tf.transpose(self.anchor_grid[i], [0, 2, 1, 3]) * 4
-                xy = (y[..., 0:2] * 2 + grid) * self.stride[i]  # xy
-                wh = y[..., 2:4] ** 2 * anchor_grid
-                # Normalize xywh to 0-1 to reduce calibration error
-                xy /= tf.constant([[self.imgsz[1], self.imgsz[0]]], dtype=tf.float32)
-                wh /= tf.constant([[self.imgsz[1], self.imgsz[0]]], dtype=tf.float32)
-                y = tf.concat([xy, wh, y[..., 4:]], -1)
+                anchor_grid = tf.split(anchor_grid, self.na, axis=2)
+
+                tmp = []
+                for j in range(self.na):
+                    xy = (y[j][..., 0:2] * 2 + grid) * self.stride[i]  # xy
+                    wh = y[j][..., 2:4] ** 2 * anchor_grid[j]
+                    # Normalize xywh to 0-1 to reduce calibration error
+                    xy /= tf.constant([[self.imgsz[1], self.imgsz[0]]], dtype=tf.float32)
+                    wh /= tf.constant([[self.imgsz[1], self.imgsz[0]]], dtype=tf.float32)
+                    tmp.append(tf.concat([xy, wh, y[j][..., 4:]], -1))
+                y = tf.concat(tmp, axis=2)
                 z.append(tf.reshape(y, [-1, self.na * ny * nx, self.no]))
+
+            x[i] = tf.reshape(x[i], [-1, ny * nx, self.na, self.no])
 
         return tf.transpose(x, [0, 2, 1, 3]) if self.training else (tf.concat(z, 1), x)
 
